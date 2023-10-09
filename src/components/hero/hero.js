543a5dd191c7e81035e6cf416ab90b9a9bc970e8
pathakhHemanta <pathakh@warhawks.ulm.edu>
2023-10-08 17:04:42 -0700

    Fixed the mapping on undefined error in the Hero Component

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -7,24 +7,25 @@ const Hero = ({ movies }) => {
   return (
     <div className="movie-carousel-container">
       <Carousel>
-        {movies.map((movie) => {
-          return (
-            <Paper>
-              <div className="movie-card-container">
-                <div className="movie-card">
-                  <div className="movie-detail">
-                    <div className="movie-poster">
-                      <img src={movie.poster} alt="" />
-                    </div>
-                    <div className="movie-title">
-                      <h4>{movie.title}</h4>
+        {movies !== undefined &&
+          movies.map((movie) => {
+            return (
+              <Paper key={movie.imdbId}>
+                <div className="movie-card-container">
+                  <div className="movie-card">
+                    <div className="movie-detail">
+                      <div className="movie-poster">
+                        <img src={movie.poster} alt="" />
+                      </div>
+                      <div className="movie-title">
+                        <h4>{movie.title}</h4>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </Paper>
-          );
-        })}
+              </Paper>
+            );
+          })}
       </Carousel>
     </div>
   );
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "../hero/hero";
 
 const Home = ({ movies }) => {
-  return <Hero movies={movies} />;
+  return (
+    <>
+      <Hero movies={movies} />
+    </>
+  );
 };
 
 export default Home;
