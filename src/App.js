1256fa32830c6cd51f7e1ca5f92447b4fc8eac54
pathakhHemanta <pathakh@warhawks.ulm.edu>
2023-10-09 23:42:06 -0700

    Cleared the index.css file Edited the Hero.css file

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   const getMovies = async () => {
     try {
       const response = await api.get("/api/v1/movies");
-      // console.log(response.data);
+      console.log(response.data);
       setMovies(response.data);
     } catch (err) {
       console.log(err);
--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./Hero.css";
+import "./Hero.css";
 import Carousel from "react-material-ui-carousel";
 import { Paper } from "@mui/material";
 
@@ -12,7 +12,10 @@ const Hero = ({ movies }) => {
             return (
               <Paper key={movie.imdbId}>
                 <div className="movie-card-container">
-                  <div className="movie-card">
+                  <div
+                    className="movie-card"
+                    style={{ "--img": `url(${movie.backdrops[0]})` }}
+                  >
                     <div className="movie-detail">
                       <div className="movie-poster">
                         <img src={movie.poster} alt="" />
